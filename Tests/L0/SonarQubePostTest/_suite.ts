1e0c599c322a2860d2c74274385a29d4f1883763
Eric Sciple <ersciple@microsoft.com>
2016-04-23 22:33:11 -0400

    PS test perf.

--- Tests/L0/SonarQubePostTest/_suite.ts
+++ Tests/L0/SonarQubePostTest/_suite.ts
@@ -7,36 +7,42 @@ import psm = require('../../lib/psRunner');
 import path = require('path');
 var shell = require('shelljs');
 var ps = shell.which('powershell');
+var psr = null;
 
 describe('SonarQubePostTest Suite', function () {
     this.timeout(20000);
 
     before((done) => {
-        // init here
+        if (ps) {
+            psr = new psm.PSRunner();
+            psr.start();
+        }
+
         done();
     });
 
     after(function () {
+        psr.kill();
     });
 
     if (ps) {
         it('UploadSumamryMdFile tests', (done) => {
-            psm.runPS(path.join(__dirname, 'UploadSumamryMdFile.ps1'), done);
+            psr.run(path.join(__dirname, 'UploadSumamryMdFile.ps1'), done);
         })
          it('DisableAnalysisOnPrBuild tests', (done) => {
-            psm.runPS(path.join(__dirname, 'DisableAnalysisOnPrBuild.ps1'), done);
+            psr.run(path.join(__dirname, 'DisableAnalysisOnPrBuild.ps1'), done);
         })
         it('TopLevelOrchestration tests', (done) => {
-            psm.runPS(path.join(__dirname, 'TopLevelOrchestration.ps1'), done);
+            psr.run(path.join(__dirname, 'TopLevelOrchestration.ps1'), done);
         })
         it('PRCA Report Processor tests', (done) => {
-            psm.runPS(path.join(__dirname, 'PRCA', 'ReportProcessorTests.ps1'), done);
+            psr.run(path.join(__dirname, 'PRCA', 'ReportProcessorTests.ps1'), done);
         })       
         it('PRCA Post Comments tests', (done) => {
-            psm.runPS(path.join(__dirname, 'PRCA', 'PostCommentsTests.ps1'), done);
+            psr.run(path.join(__dirname, 'PRCA', 'PostCommentsTests.ps1'), done);
         })       
         it('PRCA Orchestrator tests', (done) => {
-            psm.runPS(path.join(__dirname, 'PRCA', 'OrchestratorTests.ps1'), done);
+            psr.run(path.join(__dirname, 'PRCA', 'OrchestratorTests.ps1'), done);
         })       
     }
 });
